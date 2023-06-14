5aa69b57ba9ac0f61fe9901edfae74bfe742d7e4
AgustinaRios <agus.riosv@gmail.com>
2023-06-14 17:08:05 -0300

    Primeras clases

---
+++ Dominio/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Dominio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public enum TipoUsuario
+    {
+        NORMAL = 1,
+        ADMIN = 2
+    }
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Pass { get; set; }
+
+        public TipoUsuario TipoUsuario { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
