1ead3c47b0aa3bfd26575d3325365a0ba96d40b9
Gisela Suero <gisesuero@hotmail.com>
2023-06-15 00:34:47 -0300

    creacion AccesoDatos
    Se crea AccesoDatos y se cambia cnx de carpetas

--- Dominio/obj/Debug/netcoreapp3.1/1.AssemblyInfo.cs
+++ Dominio/obj/Debug/netcoreapp3.1/1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
+[assembly: System.Reflection.AssemblyProductAttribute("1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
