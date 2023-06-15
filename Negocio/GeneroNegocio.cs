ecb87a70f9a9bbc1ab3324559893c547f78a88d1
Gisela Suero <gisesuero@hotmail.com>
2023-06-15 00:52:37 -0300

    se crea GeneroNegocio

---
+++ Negocio/GeneroNegocio.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class GeneroNegocio
+    {
+
+           public List<Genero> listar()
+            {
+                List<Genero> lista = new List<Genero>();
+                AccesoDatos datos = new AccesoDatos();
+
+                try
+                {
+                    datos.setearConsulta("Select Id,Descripcion from GENEROS");
+                    datos.ejecutarLectura();
+
+                    while (datos.Lector.Read())
+                    {
+                    Genero aux = new Genero();
+                        aux.Id = (int)datos.Lector["Id"];
+                        aux.Descripcion = (string)datos.Lector["Descripcion"];
+
+                        lista.Add(aux);
+                    }
+
+                    return lista;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }
+
+            public void agregar(Categoria nuevo)
+            {
+                AccesoDatos datos = new AccesoDatos();
+                try
+                {
+                    string valores = "values( '" + nuevo.Descripcion + "')";
+                    datos.setearConsulta("insert into  CATEGORIAS (Descripcion) " + valores);
+
+                    datos.ejectutarAccion();
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }
+
+            public void EliminarFisico(Int32 Id)
+            {
+                AccesoDatos datos = new AccesoDatos();
+
+                try
+                {
+                    datos.setearConsulta("delete from GENEROS where Id = @id");
+                    datos.setearParametro("@id", Id);
+                    datos.ejectutarAccion();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    datos.cerrarConexion();
+                }
+            }
+        }
+    }
+
+
+
